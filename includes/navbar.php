73641e488d421b7e964dc4d84f9555532226152d
Audrey Schmook <audrey.schmook@gmail.com>
2018-04-01 21:06:37 -0500

    fixed navbar and played more

---
+++ includes/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Audrey Schmook</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="family.html">Family</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="hobbies.html">Hobbies
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Cloth Diapers</a></li>
+          <li><a href="#">another page</a></li>
+        </ul>
+      </li>
+      <li><a href="professional.html">Career</a></li>
+    </ul>
+  </div>
+</nav>
